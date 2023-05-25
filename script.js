a44322c4ac8fea531cb848fc9e260901c4bdb438
Sufail Salim <99598004+Sufail07@users.noreply.github.com>
2023-05-25 17:07:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function sum(a,b) {
+    var s = a + b;
+    console.log(s);
+}
+var a = parseInt(prompt("Enter the first number : "));
+var b = parseInt(prompt("Enter the second number :"));
+sum(a,b);
